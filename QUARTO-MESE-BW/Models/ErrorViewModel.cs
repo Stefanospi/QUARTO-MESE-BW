1db29a8231d84ab2afdff23db87fb73584f7dcc8
Stefanospi <sspitaleri26@gmail.com>
2024-07-08 11:20:36 +0200

    Empty

--- QUARTO-MESE-BW/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QUARTO_MESE_BW.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
