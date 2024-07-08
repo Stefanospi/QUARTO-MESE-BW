d98d30f80dadc6c253c9ec5f471077935c34ae88
Stefanospi <sspitaleri26@gmail.com>
2024-07-08 14:47:49 +0200

    commit

--- Quarto _Mese_BW/obj/Debug/net7.0/Quarto _Mese_BW.AssemblyInfo.cs
+++ Quarto _Mese_BW/obj/Debug/net7.0/Quarto _Mese_BW.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quarto _Mese_BW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+286ba6fd2ae5e4068c585c70502a7d4a5fd1d4d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cab66bc3bf2e60fa04f4345a4b2450c4851c9123")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quarto _Mese_BW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quarto _Mese_BW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
