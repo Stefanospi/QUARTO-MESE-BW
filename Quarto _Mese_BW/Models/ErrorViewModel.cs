76239137276ad4fbf40b9dba021540d0b698db28
jasminemahfouz <jasmine12mahfouz@gmail.com>
2024-07-08 11:27:15 +0200

    folder

---
+++ Quarto _Mese_BW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Quarto__Mese_BW.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
