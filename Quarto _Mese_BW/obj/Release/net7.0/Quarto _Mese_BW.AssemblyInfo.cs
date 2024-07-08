3c16e5e557fcc81228bdce0fa34cb1b18de8bfd7
sabrinacinque <cinque.sabrina@gmail.com>
2024-07-08 11:40:29 +0200

    deddsc

--- Quarto _Mese_BW/obj/Release/net7.0/Quarto _Mese_BW.AssemblyInfo.cs
+++ Quarto _Mese_BW/obj/Release/net7.0/Quarto _Mese_BW.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quarto _Mese_BW")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76239137276ad4fbf40b9dba021540d0b698db28")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quarto _Mese_BW")]
