dda27e58c53759f77314aaf2516dd650ab12aa33
sabrinacinque <cinque.sabrina@gmail.com>
2024-07-10 10:10:37 +0200

    ggbbg

--- Quarto _Mese_BW/obj/Debug/net7.0/Quarto _Mese_BW.AssemblyInfo.cs
+++ Quarto _Mese_BW/obj/Debug/net7.0/Quarto _Mese_BW.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quarto _Mese_BW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa91dab6fa932576165facf414fb2fbd4d2813a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d251f97b9a7db9907c5651e77456980314b7efac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quarto _Mese_BW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quarto _Mese_BW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
