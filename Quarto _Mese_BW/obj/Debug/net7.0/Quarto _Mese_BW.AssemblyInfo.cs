a799a8df9351d9b8973aca591fbef28938f30a39
FraIanna <francescoiannaccone9@gmail.com>
2024-07-12 10:47:39 +0200

    Piccoli aggiustamenti

--- Quarto _Mese_BW/obj/Debug/net7.0/Quarto _Mese_BW.AssemblyInfo.cs
+++ Quarto _Mese_BW/obj/Debug/net7.0/Quarto _Mese_BW.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quarto _Mese_BW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25ba576ce1a21fb6860ce10a13d7331275e5d9cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1d21a180efd7c509bb84e2774189ee85450bfa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quarto _Mese_BW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quarto _Mese_BW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
